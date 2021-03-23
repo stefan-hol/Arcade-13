86cf4e7f18b0110be0674c6aea023bacbe0a7333
stefan <stefan.hol@hotmail.com>
2021-03-23 12:09:23 +0100

    met extra ship in code

---
+++ unity/arcade13/Assets/scripts/plane1script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plane1script : MonoBehaviour
+{
+    float speed = 8.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, speed) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -speed) * Time.deltaTime;
+        }
+    }
+}
